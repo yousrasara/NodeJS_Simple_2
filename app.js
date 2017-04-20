934e2a77379adb8dbadfa2bfa24ed6a271588c67
John Quillinan <John.Quillinan@us.ibm.com>
2017-04-20 21:58:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var redis = require('redis');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -36,8 +37,10 @@ app.get('/', function(req, res){
   res.render('index', {userCount: userCount}); /* update this line to also pass userbytwo */
 });
 
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+//app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+//  console.log("server starting on " + appEnv.url);
 });
